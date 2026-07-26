09f469bf995ef8bdeb7d3ad28a1eedc8b7045a7d
Arif <dopymonster@gmail.com>
2026-07-27 03:54:06 +0800

    update

--- src/js/data/lessonData2.js
+++ src/js/data/lessonData2.js
@@ -1,291 +1,145 @@
 export let json = [
 
-	// {
-	// 	text: "Number Bonds ialah cara memecahkan satu nombor kepada dua bahagian",
-	// 	type: "Kombinasi",
-	// 	content: {
-	// 		whole: 8,
-	// 		part: [5, 3]
-	// 	},
-	// },
-	// {
-	// 	text: "Contoh 1, 6 boleh dipecahkan kepada 1 dan 5",
-	// 	type: "Kombinasi",
-	// 	content: {
-	// 		whole: 6,
-	// 		part: [1, 5]
-	// 	},
-	// },
-	// {
-	// 	text: "Contoh 2, 6 boleh dipecahkan kepada 2 dan 4",
-	// 	type: "Kombinasi",
-	// 	content: {
-	// 		whole: 6,
-	// 		part: [2, 4]
-	// 	},
-	// },
-	// {
-	// 	text: "Contoh 3, 6 boleh dipecahkan kepada 3 dan 3",
-	// 	type: "Kombinasi",
-	// 	content: {
-	// 		whole: 6,
-	// 		part: [3, 3]
-	// 	},
-	// },
-	// {
-	// 	text: "Contoh 4, 6 boleh dipecahkan kepada 4 dan 6",
-	// 	type: "Kombinasi",
-	// 	content: {
-	// 		whole: 6,
-	// 		part: [4, 2]
-	// 	},
-	// },
-	// {
-	// 	text: "Padankan soalan dengan jawapan yang betul.",
-	// 	type: "Padankan",
-	// 	content: [
-	// 		{ question: "1 + 4", answer: 5 },
-	// 		{ question: "2 + 3", answer: 5 },
-	// 		{ question: "5 + 1", answer: 6 },
-	// 		{ question: "3 + 3", answer: 6 },
-	// 	]
-	// },
-	// {
-	// 	text: "Contoh 3, Jumlah 3 dan 3 ialah 6",
-	// 	type: "KombinasiTerbalik",
-	// 	content: {
-	// 		whole: 6,
-	// 		part: [3, 3]
-	// 	},
-	// },
-	// {
-	// 	text: "Contoh 3, Jumlah 4 dan 2 ialah 6",
-	// 	type: "KombinasiTerbalik",
-	// 	content: {
-	// 		whole: 6,
-	// 		part: [4, 2]
-	// 	},
-	// },
-	// {
-	// 	text: "Contoh 3, Jumlah 5 dan 1 ialah 6",
-	// 	type: "KombinasiTerbalik",
-	// 	content: {
-	// 		whole: 6,
-	// 		part: [5, 1]
-	// 	},
-	// },
-	// {
-	// 	text: "Contoh 3, Jumlah 6 dan 0 ialah 6",
-	// 	type: "KombinasiTerbalik",
-	// 	content: {
-	// 		whole: 6,
-	// 		part: [6, 0]
-	// 	},
-	// },
-	// {
-	// 	text: "Padankan soalan dengan jawapan yang betul.",
-	// 	type: "Padankan",
-	// 	content: [
-	// 		{ question: 5, answer: "1 + 4" },
-	// 		{ question: 5, answer: "2 + 3" },
-	// 		{ question: 6, answer: "5 + 1" },
-	// 		{ question: 6, answer: "3 + 3" },
-	// 	]
-	// },
-	// {
-	// 	text: "Betulkah persamaan ini",
-	// 	type: "TrueFalse",
-	// 	content: {
-	// 		nums: [5, 5, 10]
-	// 	},
-	// 	options: ["True", "False"],
-	// 	answer: "True",
-	// }, {
-	// 	text: "Betulkah persamaan ini",
-	// 	type: "TrueFalse",
-	// 	content: {
-	// 		nums: [8, 5, 10]
-	// 	},
-	// 	options: ["True", "False"],
-	// 	answer: "False",
-	// },
-	// {
-	// 	text: "Betulkah persamaan ini",
-	// 	type: "TrueFalse",
-	// 	content: {
-	// 		nums: [3, 6, 10]
-	// 	},
-	// 	options: ["True", "False"],
-	// 	answer: "False",
-	// }, {
-	// 	text: "Betulkah persamaan ini",
-	// 	type: "TrueFalse",
-	// 	content: {
-	// 		nums: [3, 7, 10]
-	// 	},
-	// 	options: ["True", "False"],
-	// 	answer: "True",
-	// }, {
-	// 	text: "Betulkah persamaan ini",
-	// 	type: "TrueFalse",
-	// 	content: {
-	// 		nums: [2, 8, 10]
-	// 	},
-	// 	options: ["True", "False"],
-	// 	answer: "True",
-	// },
-
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "Friends of 10 ialah dua nombor yang apabila digabungkan akan menghasilkan jumlah 10",
-	// 		content: {
-	// 			nombor: 8,
-	// 			pelengkap: 2,
-	// 			showAnswer: true
-	// 		},
-	// 	},
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "Contoh 1, 9 perlukan berapa lagi untuk jadi 10?",
-	// 		content: {
-	// 			nombor: 9,
-	// 			pelengkap: 1,
-	// 			showAnswer: false
-	// 		},
-	// 	},
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "9 perlukan 1 lagi untuk jadi 10.",
-	// 		content: {
-	// 			nombor: 9,
-	// 			pelengkap: 1,
-	// 			showAnswer: true
-	// 		}
-	// 	},
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "Contoh 2, 8 perlukan berapa lagi untuk jadi 10?",
-	// 		content: {
-	// 			nombor: 8,
-	// 			pelengkap: 2,
-	// 			showAnswer: false
-	// 		},
-	// 	},
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "8 perlukan 2 lagi untuk jadi 10.",
-	// 		content: {
-	// 			nombor: 8,
-	// 			pelengkap: 2,
-	// 			showAnswer: true
-	// 		}
-	// 	},
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "Contoh 3, 7 perlukan berapa lagi untuk jadi 10?",
-	// 		content: {
-	// 			nombor: 7,
-	// 			pelengkap: 3,
-	// 			showAnswer: false
-	// 		}
-	// 	},
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "7 perlukan 3 lagi untuk jadi 10",
-	// 		content: {
-	// 			nombor: 7,
-	// 			pelengkap: 3,
-	// 			showAnswer: true
-	// 		}
-	// 	},
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "Contoh 4, 5 perlukan berapa lagi untuk jadi 10?",
-	// 		content: {
-	// 			nombor: 5,
-	// 			pelengkap: 5,
-	// 			showAnswer: false
-	// 		}
-	// 	},
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "5 perlukan 5 lagi untuk jadi 10.",
-	// 		content: {
-	// 			nombor: 5,
-	// 			pelengkap: 5,
-	// 			showAnswer: true
-	// 		}
-	// 	},
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "Contoh 5, 9 perlukan berapa lagi untuk jadi 10?",
-	// 		content: {
-	// 			nombor: 9,
-	// 			pelengkap: 1,
-	// 			showAnswer: false
-	// 		}
-	// 	},
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "9 perlukan 1 lagi untuk jadi 10.",
-	// 		content: {
-	// 			nombor: 9,
-	// 			pelengkap: 1,
-	// 			showAnswer: true
-	// 		}
-	// 	},
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "Contoh 6, 10 perlukan berapa lagi untuk jadi 10?",
-	// 		content: {
-	// 			nombor: 10,
-	// 			pelengkap: 0,
-	// 			showAnswer: false
-	// 		}
-	// 	},
-	// 	{
-	// 		type: "DiagramBox",
-	// 		text: "10 perlukan 0 lagi untuk jadi 10.",
-	// 		content: {
-	// 			nombor: 10,
-	// 			pelengkap: 0,
-	// 			showAnswer: true
-	// 		}
-	// 	},
-	// 	{
-	// 		text: "Padankan soalan dengan jawapan yang betul.",
-	// 		type: "Padankan",
-	// 		content: [
-	// 			{ question: "5 + 5", answer: 10 },
-	// 			{ question: "4 + 6", answer: 10 },
-	// 			{ question: "3 + 7", answer: 10 },
-	// 			{ question: "8 + 2", answer: 10 },
-	// 		]
-	// 	},
-	// 	{
-	// 		text: "Padankan soalan dengan jawapan yang betul.",
-	// 		type: "MemoryGame",
-	// 		content: [
-	// 			{ question: "5 + 5", answer: 10 },
-	// 			{ question: "4 + 6", answer: 10 },
-	// 			{ question: "3 + 7", answer: 10 },
-	// 			{ question: "8 + 2", answer: 10 },
-	// 			{ question: "5 + 5", answer: 10 },
-	// 			{ question: "4 + 6", answer: 10 },
-	// 			{ question: "3 + 7", answer: 10 },
-	// 			{ question: "8 + 2", answer: 10 },
-	// 		]
-	// 	},
-
-
 	{
-		text: "Mari belajar teknik pelengkap 10",
-		type: "TeknikPelengkap10",
-
+		type: "DiagramBox",
+		text: "Friends of 10 ialah dua nombor yang apabila digabungkan akan menghasilkan jumlah 10",
+		content: {
+			nombor: 8,
+			pelengkap: 2,
+			showAnswer: true
+		},
+	},
+	{
+		type: "DiagramBox",
+		text: "Contoh 1, 9 perlukan berapa lagi untuk jadi 10?",
+		content: {
+			nombor: 9,
+			pelengkap: 1,
+			showAnswer: false
+		},
+	},
+	{
+		type: "DiagramBox",
+		text: "9 perlukan 1 lagi untuk jadi 10.",
+		content: {
+			nombor: 9,
+			pelengkap: 1,
+			showAnswer: true
+		}
+	},
+	{
+		type: "DiagramBox",
+		text: "Contoh 2, 8 perlukan berapa lagi untuk jadi 10?",
+		content: {
+			nombor: 8,
+			pelengkap: 2,
+			showAnswer: false
+		},
+	},
+	{
+		type: "DiagramBox",
+		text: "8 perlukan 2 lagi untuk jadi 10.",
+		content: {
+			nombor: 8,
+			pelengkap: 2,
+			showAnswer: true
+		}
+	},
+	{
+		type: "DiagramBox",
+		text: "Contoh 3, 7 perlukan berapa lagi untuk jadi 10?",
+		content: {
+			nombor: 7,
+			pelengkap: 3,
+			showAnswer: false
+		}
+	},
+	{
+		type: "DiagramBox",
+		text: "7 perlukan 3 lagi untuk jadi 10",
+		content: {
+			nombor: 7,
+			pelengkap: 3,
+			showAnswer: true
+		}
+	},
+	{
+		type: "DiagramBox",
+		text: "Contoh 4, 5 perlukan berapa lagi untuk jadi 10?",
+		content: {
+			nombor: 5,
+			pelengkap: 5,
+			showAnswer: false
+		}
+	},
+	{
+		type: "DiagramBox",
+		text: "5 perlukan 5 lagi untuk jadi 10.",
+		content: {
+			nombor: 5,
+			pelengkap: 5,
+			showAnswer: true
+		}
+	},
+	{
+		type: "DiagramBox",
+		text: "Contoh 5, 9 perlukan berapa lagi untuk jadi 10?",
+		content: {
+			nombor: 9,
+			pelengkap: 1,
+			showAnswer: false
+		}
+	},
+	{
+		type: "DiagramBox",
+		text: "9 perlukan 1 lagi untuk jadi 10.",
+		content: {
+			nombor: 9,
+			pelengkap: 1,
+			showAnswer: true
+		}
+	},
+	{
+		type: "DiagramBox",
+		text: "Contoh 6, 10 perlukan berapa lagi untuk jadi 10?",
+		content: {
+			nombor: 10,
+			pelengkap: 0,
+			showAnswer: false
+		}
+	},
+	{
+		type: "DiagramBox",
+		text: "10 perlukan 0 lagi untuk jadi 10.",
+		content: {
+			nombor: 10,
+			pelengkap: 0,
+			showAnswer: true
+		}
+	},
+	{
+		text: "Padankan soalan dengan jawapan yang betul.",
+		type: "Padankan",
 		content: [
-			{
-				options: [8, 5]
-			},
+			{ question: "5 + 5", answer: 10 },
+			{ question: "4 + 6", answer: 10 },
+			{ question: "3 + 7", answer: 10 },
+			{ question: "8 + 2", answer: 10 },
 		]
-	}
+	},
+	{
+		text: "Padankan soalan dengan jawapan yang betul.",
+		type: "MemoryGame",
+		content: [
+			{ question: "5 + 5", answer: 10 },
+			{ question: "4 + 6", answer: 10 },
+			{ question: "3 + 7", answer: 10 },
+			{ question: "8 + 2", answer: 10 },
+			{ question: "5 + 5", answer: 10 },
+			{ question: "4 + 6", answer: 10 },
+			{ question: "3 + 7", answer: 10 },
+			{ question: "8 + 2", answer: 10 },
+		]
+	},
+
 ]
