66eb48ae308acecc1bb9c4b188f121565e2c9f99
Arif <dopymonster@gmail.com>
2026-07-27 19:14:57 +0800

    update

---
+++ src/js/data/lessonData71.js
@@ -0,0 +1,32 @@
+export const json = [
+	{
+		text: "Jawab soalan tambah",
+		type: "ChoiseQuiz",
+		content: {
+			nums: [5, ""],
+			operator: "+"
+		},
+		options:[5,6,4,3],
+		answer: 5
+	},
+	{
+		text: "Jawab soalan tambah",
+		type: "ChoiseQuiz",
+		content: {
+			nums: [5, ""],
+			operator: "+"
+		},
+		options: [5, 6, 4,5],
+		answer: 5
+	},
+	{
+		text: "Jawab soalan tambah",
+		type: "ChoiseQuiz",
+		content: {
+			nums: [5, ""],
+			operator: "+"
+		},
+		options: [5, 6, 4, 6],
+		answer: 5
+	},
+];
