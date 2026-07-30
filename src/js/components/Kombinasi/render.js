bd7e63b888c81b388e3e78adf9f26802909ad1c5
Arif <dopymonster@gmail.com>
2026-07-30 15:56:29 +0800

    Kombinasi DONE

--- src/js/components/Kombinasi/render.js
+++ src/js/components/Kombinasi/render.js
@@ -2,7 +2,7 @@ export function renderLearnKombinasi(data) {
 	return `
 		<div class="content grid-3">
 			<h1></h1>
-			<h1 class="eqn text-center garisPecah2 primary">${data.content.whole}</h1>
+			<h1 class="eqn text-center garisPecah2 primary ">${data.content.whole}</h1>
 			<h1></h1>
 			<h1 class="pecah text-center secondary">${data.content.part[0]}</h1>
 			<h1></h1>
@@ -10,11 +10,11 @@ export function renderLearnKombinasi(data) {
 		</div>
 
 		<div class="content grid-5">
-			<h1 class="text-center eqn primary">${data.content.whole}</h1>
+			<h1 class="text-center eqn primary aspect-square">${data.content.whole}</h1>
 			<h1 class="text-center">=</h1>
-			<h1 class="text-center bulat secondary">${data.content.part[0]}</h1>
+			<h1 class="text-center bulat secondary aspect-square">${data.content.part[0]}</h1>
 			<h1 class="text-center">+</h1>
-			<h1 class="text-center bulat yellow">${data.content.part[1]}</h1>
+			<h1 class="text-center bulat yellow aspect-square">${data.content.part[1]}</h1>
 		</div>
 
 	`
