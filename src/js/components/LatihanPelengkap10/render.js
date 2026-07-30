072f0c3bfd8ef4014bb5650ed073e5f0d009c6e8
Arif <dopymonster@gmail.com>
2026-07-30 16:03:47 +0800

    LatihanPelengkap10 DONE

--- src/js/components/LatihanPelengkap10/render.js
+++ src/js/components/LatihanPelengkap10/render.js
@@ -86,9 +86,9 @@ export function renderPecah(data) {
 export function renderPick(data) {
 	return `
 		<div class="content grid-3">
-			<h1 class="eqn text-center">${data.content.nums[0]}</h1>
-			<h1 class="text-center">+</h1>
-			<h1 class="eqn text-center">${data.content.nums[1]}</h1>
+			<h1 class="eqn text-center aspect-square">${data.content.nums[0]}</h1>
+			<h1 class="text-center aspect-square">+</h1>
+			<h1 class="eqn text-center aspect-square">${data.content.nums[1]}</h1>
 		</div>
 	`
 }
